18cc449c949b6eee985f7c62fa4b65c37ca7700b
Ilya <otkritie222@gmail.com>
2025-03-12 01:14:08 +0300

    нижнее меню + экраны для них

--- app/src/main/java/com/example/absolute_cinema_app/screens/ContentScreens.kt
+++ app/src/main/java/com/example/absolute_cinema_app/screens/ContentScreens.kt
@@ -13,7 +13,12 @@ import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -29,88 +34,169 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.zIndex
+import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
 import com.example.absolute_cinema_app.FilmsData.ConstansOfFilms
 import com.example.absolute_cinema_app.R
+import com.example.absolute_cinema_app.bottomNavigation.ConstantsOfBottomNav
 
 @Composable
-fun ScreenMain() {
-    val configuration = LocalConfiguration.current
-    val screenWidth = configuration.screenWidthDp.dp
-    val scrollState = rememberScrollState()
-    val imageAlpha = 1f - (scrollState.value / 500f).coerceIn(0f, 1f)
-    val backgroundColor = MaterialTheme.colorScheme.background
+fun BottomNavigationBar(navController: NavController) {
+    val items = ConstantsOfBottomNav.BottomNavItems
+    val currentRoute = navController.currentBackStackEntryAsState().value?.destination?.route
 
-    Column(
-        modifier = Modifier
-            .verticalScroll(scrollState)
-            .fillMaxSize()
-            .background(MaterialTheme.colorScheme.background)
-    ) {
-        LazyRow(
-            modifier = Modifier
-                .height(300.dp)
-                .zIndex(1f)
-        ) {
-            itemsIndexed(
-                ConstansOfFilms.FilmItems
-            ) { _, item ->
-                Box(
-                    modifier = Modifier
-                        .width(screenWidth)
-                        .height(300.dp)
-                ) {
-                    Text(
-                        text = item.label,
-                        color = Color(0xFFFFEB3B),
-                        modifier = Modifier
-                            .zIndex(3f)
-                            .fillMaxWidth()
-                            .padding(top = 235.dp)
-                            .graphicsLayer { alpha = imageAlpha },
-                        textAlign = TextAlign.Center,
-                        fontWeight = FontWeight.Bold,
-                        fontSize = 25.sp
-                    )
-                    Image(
-                        painter = painterResource(id = item.icon),
-                        contentDescription = "Background Image",
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .height(300.dp)
-                            .graphicsLayer { alpha = imageAlpha }
-                            .zIndex(1f),
-                        contentScale = ContentScale.Crop
-                    )
-                    Canvas(modifier = Modifier.fillMaxWidth()//градиент
-                        .height(300.dp).zIndex(2f)
-                        ) {
-
-                        val gradientBrush = Brush.verticalGradient(
-                            colors = listOf(backgroundColor, Color.Transparent),
-                            startY = size.height,
-                            endY = size.height - 150f
-                        )
-                        drawRect(
-                            brush = gradientBrush,
-                            size = Size(size.width, size.height)
-                        )
+    NavigationBar {
+        items.forEach { item ->
+            NavigationBarItem(
+                icon = { Icon(imageVector = item.icon, contentDescription = item.label) },
+                label = { Text(item.label) },
+                selected = currentRoute == item.route,
+                onClick = {
+                    if (currentRoute != item.route) {
+                        navController.navigate(item.route) {
+                            popUpTo(navController.graph.startDestinationId) { saveState = true }
+                            launchSingleTop = true
+                            restoreState = true
+                        }
                     }
-
-                }
-            }
-        }
-        repeat(20) {
-            Text(
-                "Item $it", modifier = Modifier.padding(16.dp),
-                color = MaterialTheme.colorScheme.onBackground
+                },
+                alwaysShowLabel = true,
+                colors = NavigationBarItemDefaults.colors(
+                    selectedIconColor = Color.Yellow,
+                    unselectedIconColor = Color.Gray,
+                    selectedTextColor = Color.Yellow,
+                )
             )
         }
     }
 }
 
 @Composable
-fun ScreenCategories() {}
+fun ScreenMain(
+    navController: NavController
+) {
+    val configuration = LocalConfiguration.current
+    val screenWidth = configuration.screenWidthDp.dp
+    val scrollState = rememberScrollState()
+    val imageAlpha = 1f - (scrollState.value / 500f).coerceIn(0f, 1f)
+    val backgroundColor = MaterialTheme.colorScheme.background
+
+    Scaffold(
+        Modifier.background(MaterialTheme.colorScheme.background),
+        bottomBar = { BottomNavigationBar(navController) }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .verticalScroll(scrollState)
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.background)
+                .padding(bottom = paddingValues.calculateBottomPadding()) // Отступ под BottomNav
+        ) {
+            LazyRow(
+                modifier = Modifier
+                    .height(300.dp)
+                    .zIndex(1f)
+            ) {
+                itemsIndexed(ConstansOfFilms.FilmItems) { _, item ->
+                    Box(
+                        modifier = Modifier
+                            .width(screenWidth)
+                            .height(300.dp)
+                    ) {
+                        Text(
+                            text = item.label,
+                            color = Color(0xFFFFEB3B),
+                            modifier = Modifier
+                                .zIndex(3f)
+                                .fillMaxWidth()
+                                .padding(top = 235.dp)
+                                .graphicsLayer { alpha = imageAlpha },
+                            textAlign = TextAlign.Center,
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 25.sp
+                        )
+                        Image(
+                            painter = painterResource(id = item.icon),
+                            contentDescription = "Background Image",
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(300.dp)
+                                .graphicsLayer { alpha = imageAlpha }
+                                .zIndex(1f),
+                            contentScale = ContentScale.Crop
+                        )
+                        Canvas(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(300.dp)
+                                .zIndex(2f)
+                        ) {
+                            val gradientBrush = Brush.verticalGradient(
+                                colors = listOf(backgroundColor, Color.Transparent),
+                                startY = size.height,
+                                endY = size.height - 150f
+                            )
+                            drawRect(
+                                brush = gradientBrush,
+                                size = Size(size.width, size.height)
+                            )
+                        }
+                    }
+                }
+            }
+            repeat(20) {
+                Text(
+                    "Item $it",
+                    modifier = Modifier.padding(16.dp),
+                    color = MaterialTheme.colorScheme.onBackground
+                )
+            }
+        }
+    }
+}
+
 
 @Composable
-fun ScreenFavorites() {}
+fun ScreenCategories(navController: NavController) {
+    Scaffold(
+        bottomBar = { BottomNavigationBar(navController) }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ) {
+            Text("Категории", modifier = Modifier.padding(16.dp))
+        }
+    }
+}
 
+@Composable
+fun ScreenFavorites(navController: NavController) {
+    Scaffold(
+        bottomBar = { BottomNavigationBar(navController) }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ) {
+            Text("Избранное", modifier = Modifier.padding(16.dp))
+        }
+    }
+}
+
+@Composable
+fun ScreenSettings(navController: NavController) {
+    Scaffold(
+        bottomBar = { BottomNavigationBar(navController) }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ) {
+            Text("Настройки", modifier = Modifier.padding(16.dp))
+        }
+    }
+}
