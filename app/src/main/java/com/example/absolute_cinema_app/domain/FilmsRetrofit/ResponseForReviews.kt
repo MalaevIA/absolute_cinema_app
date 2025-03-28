4c6b13dcc98198787f0189977155e019eacbd862
Ilya <otkritie222@gmail.com>
2025-03-28 23:49:44 +0300

    отображение комментариев к фильму и похожих фильмов

---
+++ app/src/main/java/com/example/absolute_cinema_app/domain/FilmsRetrofit/ResponseForReviews.kt
@@ -0,0 +1,4 @@
+package com.example.absolute_cinema_app.domain.FilmsRetrofit
+
+class ResponseForReviews {
+}
---
+++ app/src/main/java/com/example/absolute_cinema_app/presentation/screens/ContentScreens/Film/ReviewColumn.kt
@@ -0,0 +1,8 @@
+package com.example.absolute_cinema_app.presentation.screens.ContentScreens
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ReviewColumn(){
+
+}
