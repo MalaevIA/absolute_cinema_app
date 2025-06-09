f7f226e3a0d2805b8fae9b70560f7ef798d40958
Ilya <otkritie222@gmail.com>
2025-06-09 16:56:39 +0300

    добавление mvvm для основных функций

---
+++ app/src/main/java/com/example/absolute_cinema_app/domain/repos/FilmRepository.kt
@@ -0,0 +1,4 @@
+package com.example.absolute_cinema_app.data.repos
+
+class FilmRepository {
+}
---
+++ app/src/main/java/com/example/absolute_cinema_app/domain/viewModels/SimilarFilmsViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.absolute_cinema_app.domain.viewModels
+
+class SimilarFilmsViewModel {
+}
